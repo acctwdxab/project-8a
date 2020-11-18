ffc19ef9df2f2a36c60d48e5c81e8234d08ec25d
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-11-18 01:01:01 -0800

    Add files via upload

---
+++ count_letters.py
@@ -0,0 +1,17 @@
+# Dan Wu
+# 11/17/2020
+# Write a function that takes as a parameter a string and returns a dictionary that tabulates how many of each letter is in that string.
+import string
+
+
+def count_letters (string):
+ """takes string as parameter and returns a dictionary that tabulates each letter and its count"""
+ letter_dic = {}
+ for character in string.upper () :
+    if character in ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+        if character in letter_dic :
+                letter_dic[character] += 1
+        else :
+                    letter_dic[character] = 1
+ return letter_dic
+
